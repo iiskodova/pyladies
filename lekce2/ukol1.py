8b0afe612ffbaef9fe0c009c51427c4bf59af8d8
Irena Škodová <iiskodova@gmail.com>
2022-09-23 11:27:56 +0200

    Pyladies lesson

--- lekce2/ukol1.py
+++ lekce2/ukol1.py
@@ -1,1 +1,15 @@
-print('ahoj')
+
+from random import randrange
+cislo = randrange(3)
+tah_pocitace = cislo
+
+if cislo == 0:
+    tah_pocitace = "kámen"
+elif cislo == 1:
+    tah_pocitace = "nůžky"
+else:
+    tah_pocitace = "papír"
+    
+print(tah_pocitace)
+
+print()
