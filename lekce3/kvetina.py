a913d49da938d00590aeda6acf68a1bd23791b5e
iiskodova <113340936+iiskodova@users.noreply.github.com>
2022-10-10 10:28:36 +0200

    Update kvetina.py

--- lekce3/kvetina.py
+++ lekce3/kvetina.py
@@ -2,12 +2,7 @@ from turtle import *
 hideturtle()
 
 n = 250
-if n > 200 and n < 260:
-    pencolor('red')
-if n > 100 and n < 200:
-    pencolor('orange')
-if n > 10 and n < 100:
-    pencolor('yellow')
+pencolor('red4')
 
 while n > 5:
     for i in range (5):
@@ -16,11 +11,20 @@ while n > 5:
         left(90)
         circle(n, 90)
         left(18)
+            
     n = n - 10
 
-
-
-
-
-
-exitonclick()
+    if n > 240 :
+        pencolor('red3')
+    if n > 200 and n < 240:
+        pencolor('red1')
+    if n > 160 and n < 200:
+        pencolor('orange3')
+    if n > 120 and n < 160:
+        pencolor('orange1')
+    if n > 80 and n < 120:
+        pencolor('DarkGoldenrod3')
+    if n > 40 and n < 80:
+        pencolor('DarkGoldenrod1')
+    if n > 40 and n < 0:
+        pencolor('goldenrod1')
