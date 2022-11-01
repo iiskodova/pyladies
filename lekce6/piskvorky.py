0d78a08d4563c22914fa2e374723c5355ce25ec2
irena <iiskodova@gmail.com>
2022-11-01 19:30:51 +0100

    update ropracovane piskvorky

--- lekce6/piskvorky.py
+++ lekce6/piskvorky.py
@@ -21,4 +21,22 @@ def tah(herni_pole, cislo_pole, symbol):
     return herni_pole
 print(tah(herni_pole, 2, 'x'))
 
+def tah_hrace(herni_pole, cislo_pole, symbol):
+    while True:
+        pozice = '0'
+        while pozice not in [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20] or pozice == '':
+            pozice = input('Napiš číslo pole, na které chceš umístit symbol: ')
+            koord = seznam_tahu.get(int(pozice))
+        
+        if herni_pole[koord[0]] == '-':
+            herni_pole[koord[0]] == symbol
+            print(herni_pole)
+            return herni_pole
+        else:
+            print('Tato pozice není volná, hrej znovu.')
 
+# def seznam(herni_pole):
+#     seznam_tahu = {}
+#     i = 0
+#     for poz in range(0, 21):
+#         seznam_tahu[]
