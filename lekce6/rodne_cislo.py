3d1fcb7f0e4e1286cb8d1d4cfba8a3da9aafc3ed
irena <iiskodova@gmail.com>
2022-11-05 16:09:30 +0100

    oprava

--- lekce6/rodne_cislo.py
+++ lekce6/rodne_cislo.py
@@ -1,47 +1,27 @@
-rodne_cislo = input('Napiš své rodné číslo ve formátu xxxxxx/xxxx: ')
+rodne_cislo = ''
 
 def format_check():
-    for i in range(0, len(rodne_cislo)): 
-        if i == 6 and rodne_cislo[i] =='/': 
-            return True
-        if not rodne_cislo[i].isdigit(): 
-            return False
-print(format_check())
-
-def len_check():     
-    rc = []
-    for i in rodne_cislo:
-        cisla = i.split()
-        if i.isdigit():
-            rc.append(i)
-    if len(rc) == 10:
-        return True
-    else:
-        return False
-
-print(len_check())
-
-def divisibility():
-    if (int(rodne_cislo[0:6]) + int(rodne_cislo[7:11]))%11 == 0:
-        return True
-    else:
-        return False
-
-print(divisibility())
+    while True:
+        global rodne_cislo 
+        rodne_cislo = input('Napiš své rodné číslo ve formátu xxxxxx/xxxx: ')
+        try:
+            if len(rodne_cislo) == 11:
+                if rodne_cislo[6] == '/':
+                    numbers = rodne_cislo[:6] + rodne_cislo[7:]
+                    if int(numbers)%11 == 0:
+                        return int(numbers)
+        except ValueError:
+            print('Zkus to znovu!')
+format_check()
 
 def gender_check():
-    gender = ''
-    if int(rodne_cislo[2]) >= 5:
-        gender = 'žena'
-        return gender
-    
-    else:
-        gender = 'muž'
-        return gender
+    if int(rodne_cislo[2]) >= 5: 
+        return 'žena' 
+    else: 
+        return 'muž'
 print(gender_check())
 
 def birth_date():
-    month = []
     if int(rodne_cislo[2]) >= 5:
         month = str(int(rodne_cislo[2]) - 5) + rodne_cislo[3]
     else:
@@ -50,6 +30,4 @@ def birth_date():
     day = rodne_cislo[4:6]
     year = rodne_cislo[0:2]
     return(day, month, year)
-
-
 print(birth_date())
