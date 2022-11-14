659d41ce9afdf655d4d928ff9186c5830f7290d9
Irena Škodová <iiskodova@gmail.com>
2022-11-14 14:34:48 +0100

    opravena while true loop

--- lekce6/rodne_cislo_edit.py
+++ lekce6/rodne_cislo_edit.py
@@ -1,3 +1,27 @@
+def format_check(rodne_cislo):
+    numbers = rodne_cislo[:6] + rodne_cislo[7:]
+    if numbers.isdigit():
+        print('čísla OK')
+    if not numbers.isdigit():
+        print('zadej jen čísla a lomítko')
+        return False
+    if int(numbers) % 11 == 0:
+        print('rodné číslo je v pořádku')
+    if not int(numbers) % 11 == 0:
+        print('rodné číslo není platné')
+        return False
+    if len(rodne_cislo) == 11:
+        print('délka je správně')
+    if not len(rodne_cislo) == 11:
+        print('zadané číslo bylo moc krátké nebo dlouhé')
+        return False
+    if rodne_cislo[6] == '/':
+        print('lomítko je správně')
+        return True
+    if not rodne_cislo[6] == '/':
+        print('chybí lomítko')
+        return False
+
 def gender_check(rodne_cislo):
     if int(rodne_cislo[2]) >= 5:
         return 'žena'
@@ -16,29 +40,11 @@ def birth_date(rodne_cislo):
 
 
 if __name__ == "__main__":
-        while True:
-            rodne_cislo = input('Napiš své rodné číslo ve formátu xxxxxx/xxxx: ')
-            numbers = rodne_cislo[:6] + rodne_cislo[7:]
-            if numbers.isdigit():
-                print('čísla OK')
-            if not numbers.isdigit():
-                print('zadej jen čísla a lomítko')
-                continue
-            if int(numbers) % 11 == 0:
-                print('rodné číslo je v pořádku')
-            else:
-                print('rodné číslo není platné')
-                continue
-            if len(rodne_cislo) == 11:
-                print('délka je správně')
-            else:
-                print('zadané číslo bylo moc krátké nebo dlouhé')
-                continue
-            if rodne_cislo[6] == '/':
-                print('lomítko je správně')
-                break
-            if not rodne_cislo[6] == '/':
-                print('chybí lomítko')
-                continue
+    while True:
+        rodne_cislo = input('Napiš své rodné číslo ve formátu xxxxxx/xxxx: ')
+        if format_check(rodne_cislo) is False:
+            continue
+        else:
+            break
 print(gender_check(rodne_cislo))
 print(birth_date(rodne_cislo))
