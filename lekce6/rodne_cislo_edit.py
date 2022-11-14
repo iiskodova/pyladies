810b5dfb25d4ca4f34c561d4a8176d7b7b15371c
Irena Škodová <iiskodova@gmail.com>
2022-11-14 14:53:30 +0100

    drobne upravy formatu

--- lekce6/rodne_cislo_edit.py
+++ lekce6/rodne_cislo_edit.py
@@ -1,26 +1,20 @@
 def format_check(rodne_cislo):
     numbers = rodne_cislo[:6] + rodne_cislo[7:]
-    if numbers.isdigit():
-        print('čísla OK')
     if not numbers.isdigit():
         print('zadej jen čísla a lomítko')
         return False
-    if int(numbers) % 11 == 0:
-        print('rodné číslo je v pořádku')
     if not int(numbers) % 11 == 0:
         print('rodné číslo není platné')
         return False
-    if len(rodne_cislo) == 11:
-        print('délka je správně')
     if not len(rodne_cislo) == 11:
         print('zadané číslo bylo moc krátké nebo dlouhé')
         return False
-    if rodne_cislo[6] == '/':
-        print('lomítko je správně')
-        return True
     if not rodne_cislo[6] == '/':
         print('chybí lomítko')
         return False
+    else:
+        print('Rodné číslo je v pořádku')
+        return True
 
 def gender_check(rodne_cislo):
     if int(rodne_cislo[2]) >= 5:
@@ -42,9 +36,9 @@ def birth_date(rodne_cislo):
 if __name__ == "__main__":
     while True:
         rodne_cislo = input('Napiš své rodné číslo ve formátu xxxxxx/xxxx: ')
-        if format_check(rodne_cislo) is False:
+        if not format_check(rodne_cislo) is True:
             continue
         else:
             break
-print(gender_check(rodne_cislo))
-print(birth_date(rodne_cislo))
+    print(gender_check(rodne_cislo))
+    print(birth_date(rodne_cislo))
